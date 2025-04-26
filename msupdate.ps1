f1ca0cbd07e3307625ade8de89d6ac05a8dc0107
MadDogOwner <xiaoran@xrgzs.top>
2025-04-27 04:53:36 +0800

    fix: optimize retry logic in Invoke-UUPWebRequest

--- msupdate.ps1
+++ msupdate.ps1
@@ -63,31 +63,16 @@ function Invoke-UUPWebRequest {
         Write-Host "Attempting request to $Url (Attempt $($RetryCount + 1))..."
         $Response = Invoke-WebRequest -Uri "$Entrypoint/$Url" `
             -SkipHttpErrorCheck `
-            -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 Edg/135.0.0.0" `
-            -Headers @{
-            "authority"                 = "uupdump.net"
-            "method"                    = "GET"
-            "scheme"                    = "https"
-            "accept"                    = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
-            "accept-language"           = "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6"
-            "cache-control"             = "no-cache"
-            "pragma"                    = "no-cache"
-            "priority"                  = "u=0, i"
-            "referer"                   = "https://uupdump.net"
-            "sec-ch-ua"                 = "`"Microsoft Edge`";v=`"135`", `"Not-A.Brand`";v=`"8`", `"Chromium`";v=`"135`""
-            "sec-ch-ua-mobile"          = "?0"
-            "sec-ch-ua-platform"        = "`"Windows`""
-            "sec-fetch-dest"            = "document"
-            "sec-fetch-mode"            = "navigate"
-            "sec-fetch-site"            = "same-origin"
-            "upgrade-insecure-requests" = "1"
-        }
+            -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 Edg/135.0.0.0"
         if ($Response.Content -notmatch 'Just a moment' -and $Response.StatusCode -eq 200) {
             Write-Host "Request successful!"
             return $Response
         }
-        Write-Host "Request failed. Retrying in 3 seconds..."
-        Start-Sleep -Seconds 3
+        Write-Host "Request failed. Retrying in 1 seconds..."
+        Start-Sleep -Seconds 1
+        if ($RetryCount -ge 2) {
+            $Entrypoint = "https://uup.xrgzs.top"
+        }
         if ($RetryCount -ge 5) {
             $Entrypoint = "https://uupdump.cn"
         }
